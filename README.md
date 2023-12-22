ee0722dcda209886aede7d9195fa18ceb97e3d1c
Sinan Atıcı <aticisinancan@hotmail.com>
2023-12-22 11:48:23 +0300

    Slider Uygulaması Eklendi

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ##Basic Javascript Project
+
+1-Ankat Uygulaması
+2-Matematik Quiz Uygulaması
+3-Saat Uygulaması
+4-Todo Uygulaması
+5-Slider Uygulaması
---
+++ Slider Uygulaması/main.js
@@ -0,0 +1,36 @@
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const imageContainer = document.querySelector(".imageContainer");
+const imgs = document.querySelectorAll("img");
+
+let currentImg = 1;
+
+next.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+})
+prev.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+  
+})
+
+function updateImg() {
+  if(currentImg > imgs.length){
+    currentImg = 1;
+  }
+  else if (currentImg < 1){
+    currentImg = imgs.length;
+  }
+  imageContainer.style.transform = `translateX(-${(currentImg -1) * 700}px )`;
+  
+  timeout = setTimeout(() => {
+    currentImg++
+    updateImg();
+  },4000); 
+  
+}
+
+updateImg();
